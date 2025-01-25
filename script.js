552f85c83a1e25856ad6186b0dcd471e818c629a
guptashashankmca <165798455+guptashashankmca@users.noreply.github.com>
2025-01-25 18:45:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
+// function indexOfIgnoreCase(s1, s2) {
+//   // write your code here
+// }
+
+// // Please do not change the code below
+// const s1 = prompt("Enter s1:");
+// const s2 = prompt("Enter s2:");
+// alert(indexOfIgnoreCase(s1, s2));
 function indexOfIgnoreCase(s1, s2) {
-  // write your code here
+  // Convert both strings to lowercase for case-insensitive comparison
+  const str = s1.toLowerCase();
+  const subStr = s2.toLowerCase();
+
+  // Use indexOf to find the first occurrence of subStr in str
+  return str.indexOf(subStr);
 }
 
 // Please do not change the code below
